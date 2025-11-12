d6f906c9fa1d5206c01955531edd6aa8e7742fa5
Carlo Yannuzzi <1carlogy.dev@gmail.com>
2025-11-12 08:37:44 -0500

    initial commit with first exercise in introduction section

---
+++ introduction/max_value.rs
@@ -0,0 +1,23 @@
+use std::i32::MIN;
+
+fn max_value(nums: Vec<i32>) -> i32 {
+    let mut max = MIN;
+    for x in nums.iter() {
+        if <i32 as Into<i32>>::into(*x) > max {
+            max = (*x).into();
+        }
+    }
+
+    return max;
+}
+
+#[test]
+fn test_max_value() {
+    assert_eq!(max_value(vec![4, 7, 2, 8, 10, 9]), 10);
+    assert_eq!(max_value(vec![10, 5, 40, 45]), 45);
+    assert_eq!(max_value(vec![-5, -2, -1, -11]), -1);
+    assert_eq!(max_value(vec![42]), 42);
+    assert_eq!(max_value(vec![1000, 8]), 1000);
+    assert_eq!(max_value(vec![1000, 8, 9000]), 9000);
+    assert_eq!(max_value(vec![2, 5, 1, 1, 4]), 5);
+}
